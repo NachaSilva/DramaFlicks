cbccc48e05b8d81ef94c7e1e0821f00e54b75bca
NachaSilva <110260545+NachaSilva@users.noreply.github.com>
2023-10-19 16:55:13 -0300

    Create fetch request, styles and structure home, show batman list and static images in banner

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,36 +1,18 @@
-import { useEffect, useState } from "react";
-import { GetMovies } from "../services/TokenShowMovies";
-import { showMovies } from "../models/movies";
-
+import { MoviesHome } from './MoviesHome';
+// import { GetMovies } from "../services/TokenShowMovies";
+// import { showMovies } from "../models/movies";
 
 export const Home = () => {
+  // const API_URL = 'http://api.themoviedb.org/3'
+  // const API_KEY = 'ce9a8ba9fbedd1b69dad102a695950c2'
+  // const IMAGE_PATH = 'https://image.tmdb.org/t/p/original'
+  // const URL_IMAGE = 'https://image.tmdb.org/t/p/original'
 
-// const API_URL = 'http://api.themoviedb.org/3'
-// const API_KEY = 'ce9a8ba9fbedd1b69dad102a695950c2'
-// const IMAGE_PATH = 'https://image.tmdb.org/t/p/original'
-const URL_IMAGE = 'https://image.tmdb.org/t/p/original'
-
-//VARIABLES DE ESTADO
-
-const [movies, setMovies] = useState <showMovies[]>([])
-// const [serachKey, setSearchKey] = useState("") 
-// const [trailer, setTrailer] = useState(null)
-// const [movie, setMovie] = useState({title})
-
-
-useEffect(()=>{
-  GetMovies()
-  .then((response) => response.json())
-  .then((data) =>{setMovies(data);
-  })
-  .catch((e) => {
-    console.log('catch error', e);
-  });
-}, []);
+  // //VARIABLES DE ESTADO
 
   return (
     <>
-      <div className="header">
+      <div className='header'>
         <img className='logo' src='public\img\logo.png' alt='logo imagen' />
         <nav className='navbar navbar-light'>
           <form className='form-inline'>
@@ -44,35 +26,20 @@ useEffect(()=>{
         </nav>
         <svg
           xmlns='http://www.w3.org/2000/svg'
-          width='30'
-          height='30'
+          width='40'
+          height='40'
           fill='currentColor'
           className='bi bi-list text-light'
           viewBox='0 0 16 16'
         >
           <path
-            fill-rule='evenodd'
+            fillRule='evenodd'
             d='M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z'
           />
         </svg>
       </div>
-      <hr className="hr"/>
 
-      <div>
-      <div className="container mt-3">
-        <div className="row">
-        {/* {movies.map((movie)=>(
-         <div className="col-md-4 mb-3" >
-       
-          
-         </div>  */}
-
-
-        {/* ))} */}
-
-        </div>
-      </div>
-      </div>
+      <MoviesHome />
     </>
   );
 };
---
+++ src/components/MoviesHome.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+import { Image } from 'react-bootstrap';
+
+export const MoviesHome = () => {
+  const [movies, setMovies] = useState<MovieData | null>(null);
+
+  useEffect(() => {
+    fetch('https://www.omdbapi.com/?i=tt3896198&apikey=601d6a0c&s=batman')
+      .then((response) => response.json())
+      .then((movies) => setMovies(movies));
+  }, []);
+
+  interface MovieData {
+    Search: {
+      Title: string;
+      Year: string;
+      imdbID: string;
+      Poster: string;
+      Genre: string;
+
+      // Otras propiedades de una película
+    }[];
+    totalResults: string;
+    Response: string;
+  }
+
+  console.log(movies);
+  return (
+    <>
+    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="public\img\imageMovieBannerOne.avif" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="public\img\imageMovieBannerTwo.jpg" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="public\img\imageMovieBannerThree.avif" className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
+    <h2 className='titleCategories'>Acción</h2>
+      <div className='movies'>
+        {movies &&
+          movies.Search.map((movie) => (
+            <div className='movieDiv' key={movie.imdbID}>
+              <img className='movieImage' src={movie.Poster} alt='Imagen Película' />
+            </div>
+            
+          ))}
+          </div>
+      
+    </>
+  );
+};
+
+{
+  /* <ul>
+{movies &&
+  movies.Search.map((movie) => (
+    <li key={movie.imdbID}>
+      <div>Title: {movie.Title}</div>
+      <div><img src={movie.Poster}/></div>
+    </li>
+  ))}
+</ul> */
+}
